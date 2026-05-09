f8860f6fbf2c29c26a3e41502b715d46b87ee9ab
Click Studios Digital <ioanclickmihalca@gmail.com>
2026-05-09 21:05:58 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 cd ~/domains/saxomike.ro/public_html
 
 git pull origin main
-/opt/alt/php83/usr/bin/php composer.phar install --no-dev --optimize-autoloader
-/opt/alt/php83/usr/bin/php artisan migrate --force
-/opt/alt/php83/usr/bin/php artisan config:cache
-/opt/alt/php83/usr/bin/php artisan route:cache
-/opt/alt/php83/usr/bin/php artisan view:cache
+/opt/alt/php84/usr/bin/php composer.phar install --no-dev --optimize-autoloader
+/opt/alt/php84/usr/bin/php artisan migrate --force
+/opt/alt/php84/usr/bin/php artisan config:cache
+/opt/alt/php84/usr/bin/php artisan route:cache
+/opt/alt/php84/usr/bin/php artisan view:cache
